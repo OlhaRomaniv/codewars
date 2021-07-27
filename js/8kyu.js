2d4ab3ae98c6cfe2d4918b32957e8fb3c68acfaa
OlhaRomaniv <olharomaniv0415@gmail.com>
2021-07-27 19:30:14 +0300

    tasks js

---
+++ js/8kyu.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+//Messi goals function
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+
+ //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+ //Terminal game move function
+ function move (position, roll) {
+    return position + roll * 2;
+   }
+//https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+//Keep Hydrated!
+function litres(time) {
+    return Math.floor (time * 0.5);
+  }
+ 
