5af619fba1ce6bda955c95e7bb77a0ffc98964d7
OlhaRomaniv <olharomaniv0415@gmail.com>
2021-07-28 20:49:18 +0300

    tasks-js

--- js/8kyu.js
+++ js/8kyu.js
@@ -1,17 +1,46 @@
 //https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 //Messi goals function
+
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
     }
 
+
+
  //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
  //Terminal game move function
+
  function move (position, roll) {
     return position + roll * 2;
    }
+
+
 //https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 //Keep Hydrated!
+
 function litres(time) {
     return Math.floor (time * 0.5);
   }
+
+
+ //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+// Convert a string to an array
+function stringToArray(str) {
+   let arr = str.split(" ");
+   return arr;
+ }
+
+
+ //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+ //DNA to RNA Conversion
+ function DNAtoRNA(dna) {
+   return dna.replace(/T/g, "U");
+ }
+
+
+ //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+ //Find Maximum and Minimum Values of a List
+  const min = list => Math.min(...list);
+  const max = list => Math.max(...list);
+  
  
