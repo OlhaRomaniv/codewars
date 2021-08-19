35b72a6fa77b44a45cf4931e8fd622c424ebdb38
OlhaRomaniv <olharomaniv0415@gmail.com>
2021-08-19 21:30:37 +0300

    task-7kyu

--- js/7kyu.js
+++ js/7kyu.js
@@ -10,3 +10,29 @@ function getCount(str) {
 
     return vowelsCount;
 }
+
+//https://www.codewars.com/kata/56f935002e6c0d55fa000d92
+//Fun with ES6 Classes #2 - Animals and Inheritance
+class Shark extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 0, 'shark', status);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 4, "cat", status);
+    }
+    introduce() {
+        return super.introduce() + "  Meow meow!";
+    }
+}
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+        super(name, age, 4, "dog", status);
+        this.master = master;
+    }
+    greetMaster() {
+        return `Hello ${this.master}`;
+    }
+}
