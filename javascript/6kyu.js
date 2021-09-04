9638076900ef77edd5cdbcb016b81e6b9e951ac4
OlhaRomaniv <olharomaniv0415@gmail.com>
2021-09-04 14:58:58 +0300

    js

--- javascript/6kyu.js
+++ javascript/6kyu.js
@@ -43,3 +43,19 @@ function solution(number) {
 function order(words) {
     return words.split(' ').sort((a, b) => a.match(/\d/) - b.match(/\d/)).join(' ');
 }
+
+//https://www.codewars.com/kata/54da5a58ea159efa38000836
+//Find the odd int
+function findOdd(A) {
+    let count = 0;
+    for (let i = 0; i < A.length; i++) {
+        for (let j = 0; j < A.length; j++) {
+            if (A[i] === A[j]) {
+                count++
+            }
+        }
+        if (count % 2 !== 0) {
+            return A[i]
+        }
+    }
+}
