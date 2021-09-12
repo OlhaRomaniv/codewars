3b342137b139301c6ca87e3d16963d0e8f0d9155
OlhaRomaniv <olharomaniv0415@gmail.com>
2021-09-12 14:36:38 +0300

    js-5

--- javascript/5kyu.js
+++ javascript/5kyu.js
@@ -3,3 +3,19 @@
 var moveZeros = function(arr) {
     return arr.filter((i) => i !== 0).concat(arr.filter((i) => i === 0))
 }
+
+//https://www.codewars.com/kata/52449b062fb80683ec000024
+//The Hashtag Generator
+function generateHashtag(str) {
+    let tag = "#";
+    const words = str.split(' ').filter((char) => char !== '');
+    if (words.length === 0) {
+        return false;
+    }
+    tag += words.map((word) => { return word.charAt(0).toUpperCase() + word.slice(1) }).join('');
+    if (tag.length > 140) {
+        return false;
+    } else {
+        return tag;
+    }
+}
